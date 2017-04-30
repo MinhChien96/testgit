623e6854ee4dcdca5f0b3fdd5c1cf9aeae5b437c
MinhChien8496 <nguyenminhchien080496@gmail.com>
2017-04-30 09:42:25 +0700

    form sửa

--- QuanLyThuVien/FormDangNhap.cs
+++ QuanLyThuVien/FormDangNhap.cs
@@ -16,5 +16,10 @@ namespace QuanLyThuVien
         {
             InitializeComponent();
         }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            //ghhkjkjlkjkj
+        }
     }
 }
